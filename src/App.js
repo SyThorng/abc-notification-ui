fdaec7301f5675c697dd8e6f20e65d7081655057
Thorng <thorng@Thorngs-MacBook-Pro.local>
2026-05-09 15:43:49 +0700

    testing ngrok

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { usePushNotifications } from './hooks/usePushNotifications';
 import './App.css';
 
 const KHR_RATE = 4100;
-const NOTIFICATION_API = 'http://localhost:8081/api/test';
+const NOTIFICATION_API = 'https://mankind-juncture-paper.ngrok-free.dev/api/test';
 const TELEGRAM_BOT_USERNAME = 'ABC_BANK_Notification_Bot';
 
 function formatUSD(amount) {
